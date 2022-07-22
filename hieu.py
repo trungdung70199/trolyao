bdd07821fde23efd528f65284ca601a718b11212
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2022-07-22 13:28:32 +0900

    AI co ban

---
+++ hieu.py
@@ -0,0 +1,12 @@
+#  hieu.py
+you = "hello"
+
+if you == "":
+    robot_brain = "I can't hear you, try again"
+elif you == "hello":
+    robot_brain = "Hello Dung"
+elif you == "today":
+    robot_brain = "thu 6"
+else:
+    robot_brain = "I'm fine thank you and you"
+print(robot_brain)
---
+++ noi.py
@@ -0,0 +1,10 @@
+# noi.py
+import pyttsx3
+
+robot_brain = "I can't hear you, try again"
+
+robot_mouth= pyttsx3.init()
+
+robot_mouth.say(robot_brain)
+
+robot_mouth.runAndWait()
